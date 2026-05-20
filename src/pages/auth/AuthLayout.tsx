92d8db60a790f793552d98bafd76851ba4881dbe
huy.tranminhcs0707@hcmut.edu.vn <huy.tranminhcs0707@hcmut.edu.vn>
2026-05-20 11:42:00 +0700

    fix: add resused PageContainer Component

---
+++ src/pages/auth/AuthLayout.tsx
@@ -0,0 +1,44 @@
+// src/pages/auth/AuthLayout.tsx
+import React from 'react';
+import Header from '../../components/ui/Header';
+import AuthBanner from './components/AuthBanner';
+import { ArrowLeft } from 'lucide-react';
+import { PageContainer } from '../../components/ui/PageContainer';
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+  return (
+    <PageContainer>
+      <Header />
+
+      <div className="flex-1 flex flex-col md:flex-row">
+        {/* Left Column */}
+        <AuthBanner />
+
+        {/* Right Column: Form */}
+        <div className="w-full md:w-1/2 flex items-center justify-center p-8 bg-white relative">
+          <button
+            type="button"
+            className="absolute top-8 left-8 md:top-12 md:left-12 flex items-center text-pink-400 hover:text-pink-500 font-medium cursor-pointer transition-colors text-[15px]"
+          >
+            <ArrowLeft
+              className="w-4 h-4 mr-2"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              viewBox="0 0 24 24"
+            />
+            Back
+          </button>
+          <div className="w-full max-w-[420px] flex flex-col py-10">
+            {children}
+          </div>
+        </div>
+      </div>
+    </PageContainer>
+  );
+};
+
+export default AuthLayout;
