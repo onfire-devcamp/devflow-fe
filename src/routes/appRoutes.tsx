9da35672637f8a2708e59ca8183297bba98606ee
dhp <dohuuphuoc2303@gmail.com>
2026-05-24 02:03:22 +0700

    feat: implement PublicRoute component for handling user authentication in routes

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -3,12 +3,15 @@ import { PATHS } from '../config/paths';
 import { LoginPage } from '../features/auth/components/LoginPage';
 import ProfilePage from '../features/profile/components/ProfilePage';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 export function AppRoutes() {
   return (
     <Routes>
       {/* PUBLIC ROUTES*/}
-      <Route path={PATHS.LOGIN} element={<LoginPage />} />
-      <Route path={PATHS.REGISTER} element={<div>Register page</div>} />
+      <Route element={<PublicRoute />}>
+        <Route path={PATHS.LOGIN} element={<LoginPage />} />
+        <Route path={PATHS.REGISTER} element={<div>Register page</div>} />
+      </Route>
       <Route path="/" element={<Navigate to="/login" replace />} />
       {/*  PRIVATE ROUTES */}
       <Route element={<PrivateRoute />}>
