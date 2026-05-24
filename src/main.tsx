f6993d92826e76e24c5a71907aba53c8ebca2c5d
Nhu Phan <s4117555@rmit.edu.vn>
2026-05-24 19:27:32 +0700

    chore(config): wrap app in GoogleOAuthProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './app/index.css';
 import App from './app/App.tsx';
 
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID as string;
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 );
