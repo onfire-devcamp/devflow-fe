3d4e8a73f9c642a2747b1426f51f7ff43a0280a5
Nhu Phan <s4117555@rmit.edu.vn>
2026-05-24 15:43:36 +0700

    feat(register): wire register route

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,16 +1,18 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { PATHS } from '../config/paths';
 import { LoginPage } from '../features/auth/components/LoginPage';
+import { RegisterPage } from '../features/auth/components/RegisterPage';
 import ProfilePage from '../features/profile/components/ProfilePage';
 import { PrivateRoute } from './PrivateRoute';
 import { PublicRoute } from './PublicRoute';
+
 export function AppRoutes() {
   return (
     <Routes>
       {/* PUBLIC ROUTES*/}
       <Route element={<PublicRoute />}>
         <Route path={PATHS.LOGIN} element={<LoginPage />} />
-        <Route path={PATHS.REGISTER} element={<div>Register page</div>} />
+        <Route path={PATHS.REGISTER} element={<RegisterPage />} />
       </Route>
       <Route path="/" element={<Navigate to="/login" replace />} />
       {/*  PRIVATE ROUTES */}
